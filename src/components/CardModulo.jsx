c5a01197b953857207d5508b99bfce437686fd61
henriqueportosousa <henrique.sousa4@aluno.cps.sp.gov.br>
2026-08-19 09:55:04 -0300

    Cria os primeiros componentes React

---
+++ src/components/CardModulo.jsx
@@ -0,0 +1,12 @@
+function CardModulo({ titulo, descricao}) {
+    return (
+        <article className="card-modulo">
+            <h2>{titulo}</h2>
+            <p>{descricao}</p>
+        </article>
+    )
+}
+
+
+
+export default CardModulo
