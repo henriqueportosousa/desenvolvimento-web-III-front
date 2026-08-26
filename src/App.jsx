073e5078c47f438ef9541afcfcf7e0c0dea7c0ad
henriqueportosousa <henrique.sousa4@aluno.cps.gov.sp.br>
2026-08-26 14:13:12 -0300

    aplicaco arrumado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import './App.css'
-
-import Cabecalho from './components/Cabecalho'
-import CardModulo from './components/CardModulo'
-
+import "./App.css";
+import Cabecalho from "./components/Cabecalho";
+import CardModulo from "./components/CardModulo";
 function App() {
   return (
-    <div className='aplicativo'>
-      <main className='conteudo-principal'>
-        <Cabecalho />
-        <p className='introducao'>
+    <div className="aplicacao">
+      <Cabecalho />
+      <main className="conteudo-principal">
+        <p className="introducao">
           Aplicação desenvolvida nas disciplinas de Desenvolvimento Web III e
           Tópicos de Programação II.
         </p>
-        <section className='modulos'>
+
+        <section className="modulos">
           <CardModulo
             titulo="Gerenciamento de Produtos"
             descricao="Cadastre e consulte os produtos disponíveis."
           />
-
           <CardModulo
             titulo="Gerenciamento de Clientes"
             descricao="Cadastre e consulte os clientes da empresa."
           />
-
           <CardModulo
-            titulo="Gerenciamento de Funcionarios"
+            titulo="Gerenciamento de Funcionários"
             descricao="Cadastre e consulte os funcionários da empresa."
           />
         </section>
+
       </main>
     </div>
-  )
+  );
 }
-export default App
+export default App;
